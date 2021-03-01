a4163598e0857e41c463f9cc8c18f136f2ea0442
michael-misan <misan.miha@gmail.com>
2021-03-01 15:54:32 +0200

    New task, edit, create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,52 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:edit, :show, :destroy, :update]
+  before_action :task, only: [:new, :create]
+
   def index
-    @tasks = Task.all
+    @tasks = Task.order(created_at: :desc)
   end
 
   def destroy
-    Task.find(params[:id]).destroy
+    @task.destroy
     redirect_to tasks_path
+  end
+
+  def create
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      redirect_to new_tasks_path(@task)
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      redirect_to edit_tasks_path(@task)
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  private
+
+  def find_task
+    @task ||= Task.find(params[:id])
+  end
+
+  def task
+    @task ||= Task.new
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
